469ab1b0c0fd5005813fd80465baca52e839c23f
KeranYe <38872265+KeranYe@users.noreply.github.com>
2019-12-11 05:16:00 -0800

    Update change_perms_pfc_demo.sh

--- scripts/change_perms_pfc_demo.sh
+++ scripts/change_perms_pfc_demo.sh
@@ -2,14 +2,14 @@
 # 2019-08-16 LLW shell script for changing ownership and sticky bit for puppy_bbbl
 # usage: ~/bin/edumip_change_perms.sh
 #
-echo ls -l ~/catkin_ws/devel/lib/puppy_bbbl/pfc_demo
-ls -l ~/catkin_ws/devel/lib/puppy_bbbl/pfc_demo
+echo ls -l ~/catkin_ws/devel/lib/puppy_bbbl/puppy_bbbl_pfc_demo
+ls -l ~/catkin_ws/devel/lib/puppy_bbbl/puppy_bbbl_pfc_demo
 
-echo sudo chown root:root  ~/catkin_ws/devel/lib/puppy_bbbl/pfc_demo
-sudo chown root:root  ~/catkin_ws/devel/lib/puppy_bbbl/pfc_demo
+echo sudo chown root:root  ~/catkin_ws/devel/lib/puppy_bbbl/puppy_bbbl_pfc_demo
+sudo chown root:root  ~/catkin_ws/devel/lib/puppy_bbbl/puppy_bbbl_pfc_demo
 
-echo sudo chmod u+s  ~/catkin_ws/devel/lib/puppy_bbbl/pfc_demo
-sudo chmod u+s  ~/catkin_ws/devel/lib/puppy_bbbl/pfc_demo
+echo sudo chmod u+s  ~/catkin_ws/devel/lib/puppy_bbbl/puppy_bbbl_pfc_demo
+sudo chmod u+s  ~/catkin_ws/devel/lib/puppy_bbbl/puppy_bbbl_pfc_demo
 
-echo ls -l ~/catkin_ws/devel/lib/puppy_bbbl/pfc_demo
-ls -l ~/catkin_ws/devel/lib/puppy_bbbl/pfc_demo
+echo ls -l ~/catkin_ws/devel/lib/puppy_bbbl/puppy_bbbl_pfc_demo
+ls -l ~/catkin_ws/devel/lib/puppy_bbbl/puppy_bbbl_pfc_demo
